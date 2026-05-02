632c2e275d621625da28a29f21553c508c996caa
Jay/Fienna Liang <roackb2@gmail.com>
2026-05-02 11:51:37 +0800

    Fix sessions screen lint

--- src/web/features/control-plane/screens/SessionsScreen.tsx
+++ src/web/features/control-plane/screens/SessionsScreen.tsx
@@ -131,7 +131,6 @@ export function SessionsScreen({
     workspaceChanges,
     workspaceChangesLoading,
     workspaceChangesError,
-    selectedWorkspaceFile,
     workspaceFileDiff,
     workspaceFileDiffsByPath,
     workspaceFileDiffLoading,
